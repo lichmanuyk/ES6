a76625699326a2be7b8586505b431a0835b2d462
Pavel Lichmanuyk <pavel.lichmanuyk@gmail.com>
2018-09-08 23:18:47 +0300

    11.Объекты(objects)

---
+++ src/objects.js
@@ -0,0 +1,54 @@
+let firstName = 'Bill',
+    lastName = 'Gates',
+    email = 'billgates@microsoft.com';
+
+
+let person = {
+  firstName,
+  lastName,
+  email,
+  sayHello() {
+    console.log(`Hi, my name is ${firstName} ${lastName}`);
+  },
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  },
+  set fullName(value) {
+    this.firstName = value;
+  }
+};
+
+//ES5
+// Object.defineProperty(person, 'fullName', {
+//   get: function() {
+//     return this.firstName + ' ' + this.lastName;
+//   },
+//   set: function(value) {
+//     this.firstName = value;
+//   }
+// })
+
+console.log(person);
+// person.sayHello();
+
+// person.firstName;
+// person['firstName'];
+
+// let property = 'firstName';
+// person[property]; //person['firstName'];
+// person = {
+//   [property]: 'Bill'
+// };
+
+//ES6
+// function createCar(property, value) {
+//   return {
+//     [property]: value,
+//     ['_'+ property]: value,
+//     [property.toUpperCase()]: value,
+//     ['get' + property]() {
+//       return this[property];
+//     }
+//   }
+// }
+// console.log(createCar('vin', 1));
