cc6715b9442fedcc57a5ce4cc1844a709190fadc
Pavel Lichmanuyk <pavel.lichmanuyk@gmail.com>
2018-09-10 12:02:08 +0300

    13.Наследование(inheritance)

---
+++ src/inheritance.js
@@ -0,0 +1,53 @@
+class Task {
+  constructor(title) {
+    this._title = title;
+    this.done = false;
+    Task.count += 1;
+    console.log('Создание задачи');
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(value) {
+    this._title = value;
+  }
+
+  static getDefaultTitle() {
+    return 'Задача';
+  }
+
+  complete() {
+    this.done = true;
+    console.log(`Задача "${this.title}" выполнена.`);
+  }
+}
+Task.count = 0;
+
+class SubTask extends Task {
+  constructor(title, parent) {
+    super(title);
+    this.parent = parent;
+    console.log('Создание подзадачи');
+  }
+
+  complete() {
+    super.complete();
+    console.log(`Подзадача "${this.title}" выполнена.`)
+  }
+}
+
+let task = new Task('Изучить JavaScript');
+let subtask = new SubTask('Изучить ES6', task);
+
+//Унаследованные статические метод и свойство.
+console.log(SubTask.getDefaultTitle());
+console.log(SubTask.count);
+
+//Унаследованный метод
+task.complete();
+subtask.complete();
+
+console.log(task);
+console.log(subtask);
